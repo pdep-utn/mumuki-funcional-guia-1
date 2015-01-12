621ed5dfe97e2f2cc82bf7d3967dec62438262a0
Agustin Pina <aguspina87@gmail.com>
2015-01-11 22:29:27 -0300

    Fix test

--- 402_aplicar2-upla/test.hs
+++ 402_aplicar2-upla/test.hs
@@ -3,7 +3,7 @@ describe "aplicar2-upla" $ do
     it "aplicar((3+),(2*)) 8 = (11, 16)" $ do
       aplicar((3+),(2*)) 8  `shouldBe` (11, 16)
 
-    it "aplicar (head, length) [1, 2] = (1, 2)" $ do
+    it "aplicar ((3+),(2*)) 5 = (8, 10)" $ do
       aplicar ((3+),(2*)) 5  `shouldBe` (8, 10)
 
   
