d131b085dd792019766902870e2a4535232cc684
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-03-13 18:21:37 -0300

    Update test.hs

--- 406_esMayorDeEdad/test.hs
+++ 406_esMayorDeEdad/test.hs
@@ -1,6 +1,6 @@
 describe "esMayorDeEdad" $ do
-  it "esMayorDeEdad (fernando,25) == si es mayor de edad" $ do
-    esMayorDeEdad (fernando,25) `shouldBe` True
+  it "esMayorDeEdad (\"fernando\",25) == si es mayor de edad" $ do
+    esMayorDeEdad ("fernando",25) `shouldBe` True
 	
-  it "esMayorDeEdad (juan,15) == no es mayor de edad" $ do
-    esMayorDeEdad (juan,15) `shouldBe` False
+  it "esMayorDeEdad (\"juan\",15) == no es mayor de edad" $ do
+    esMayorDeEdad ("juan",15) `shouldBe` False
