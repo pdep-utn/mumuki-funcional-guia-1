a6d738c4da1858e1ed6115713e89f01edb5600d7
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-04-10 11:49:26 -0300

    Update test.hs

--- 405_notasFinales2-upla/test.hs
+++ 405_notasFinales2-upla/test.hs
@@ -7,8 +7,8 @@ describe "notasFinales2-upla" $ do
       notasFinales ((-1,5),(4,-1)) `shouldBe` (4,5)
 
   describe "recuperoDeGusto" $ do
-    it "recuperoDeGusto ((4,8),(5,10)) == False" $ do
-      recuperoDeGusto ((4,8),(5,10)) `shouldBe` False
+    it "recuperoDeGusto ((2,8),(5,10)) == False" $ do
+      recuperoDeGusto ((2,8),(5,10)) `shouldBe` False
 
     it "recuperoDeGusto ((6,8),(8,10)) == True" $ do
        recuperoDeGusto ((6,8),(8,10)) `shouldBe` True
