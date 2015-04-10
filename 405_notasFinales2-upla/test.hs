6cc01ffaad579eb6730ca5f2d42c8b67aa5973c5
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-04-10 11:45:44 -0300

    Update test.hs

--- 405_notasFinales2-upla/test.hs
+++ 405_notasFinales2-upla/test.hs
@@ -1,14 +1,14 @@
 describe "notasFinales2-upla" $ do
   describe "notasFinales" $ do
-    it "notasFinales ((2,8),(3,10)) == (8,10)" $ do
-      notasFinales ((2,8),(3,10)) `shouldBe` (8,10)
+    it "notasFinales ((2,8),(3,10)) == (3,10)" $ do
+      notasFinales ((2,8),(3,10)) `shouldBe` (3,10)
 
-    it "notasFinales ((-1,5),(4,-1)) == (5,4)" $ do
-      notasFinales ((-1,5),(4,-1)) `shouldBe` (5,4)
+    it "notasFinales ((-1,5),(4,-1)) == (4,5)" $ do
+      notasFinales ((-1,5),(4,-1)) `shouldBe` (4,5)
 
   describe "recuperoDeGusto" $ do
-    it "recuperoDeGusto ((2,8),(3,10)) == False" $ do
-      recuperoDeGusto ((2,8),(3,10)) `shouldBe` False
+    it "recuperoDeGusto ((4,8),(5,10)) == False" $ do
+      recuperoDeGusto ((4,8),(5,10)) `shouldBe` False
 
     it "recuperoDeGusto ((6,8),(8,10)) == True" $ do
        recuperoDeGusto ((6,8),(8,10)) `shouldBe` True
